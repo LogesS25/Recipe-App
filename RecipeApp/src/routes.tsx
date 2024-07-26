a9207ab4311b5ca95e771f665f5ab2206003d7ca
Loges <sivalog25@gmail.com>
2024-07-26 18:54:44 +0530

    protected Routes setup

--- RecipeApp/src/routes.tsx
+++ RecipeApp/src/routes.tsx
@@ -6,37 +6,44 @@ import Home from "./pages/home";
 import CreateRecipe from "./pages/post";
 import Profile from "./pages/profile";
 import Myrecipes from "./pages/myrecipes";
+import ProtectedRoutes from "./components/ui/protectedRoutes";
 
 export const router = createBrowserRouter([
     {
-        path:"/home",
-        element:<Home/>,
-        errorElement:<Error/>,
-    },{
-        path:"/post",
-        element:<CreateRecipe/>,
-        errorElement:<Error/>,
-    },
-    {
-        path:"/login",
-        element:<Login/>,
-        errorElement:<Error/>,
-    },
-    {
-        path:"/profile",
-        element:<Profile/>,
-        errorElement:<Error/>,
-    }, 
-    {
-        path:"/myrecipes",
-        element:<Myrecipes/>,
-        errorElement:<Error/>,
-    }, 
-    {
-        path:"/signup",
-        element:<Signup/>,
-        errorElement:<Error/>,
-    }
-]);
-
-export default router;
+        element: <ProtectedRoutes />,
+        children: [
+          {
+            path: "/",
+            element: <Home />,
+            errorElement: <Error />,
+          },
+          {
+            path: "/post",
+            element: <CreateRecipe />,
+            errorElement: <Error />,
+          },
+          {
+            path: "/profile",
+            element: <Profile />,
+            errorElement: <Error />,
+          },
+          {
+            path: "/myrecipes",
+            element: <Myrecipes />,
+            errorElement: <Error />,
+          },
+        ],
+      },
+      {
+        path: "/login",
+        element: <Login />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+        errorElement: <Error />,
+      },
+    ]);
+    
+    export default router;
